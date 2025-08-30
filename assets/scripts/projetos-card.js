28eea9000d5e30f80705d6f633ee5555fc5401b1
Peixoto1990 <aluilson_peixoto@hotmail.com>
2025-08-30 13:55:17 -0300

    Adicionando seção de créditos, modificando estilos e finalizando estrutura

--- assets/scripts/projetos-card.js
+++ assets/scripts/projetos-card.js
@@ -1,7 +1,7 @@
 function criaDados() {
     const dataCards = [
         {
-            "id": 0,
+            "id": 1,
             "nome": "Organo",
             "capa": "assets/images/projetos-card/organo.png",
             "descricao": "Organo é um aplicativo desenvolvido com ReactJS. Trata-se de um app que cadastra e gerencia equipes de colaboradores em uma determinada empresa.",
@@ -32,11 +32,11 @@ function criaDados() {
             "video": "assets/videos/organo-video.webm"
         },
         {
-            "id": 1,
-            "nome": "Organo",
-            "capa": "assets/images/projetos-card/organo.png",
-            "descricao": "Organo é um aplicativo desenvolvido com ReactJS. Trata-se de um app que cadastra e gerencia equipes de colaboradores em uma determinada empresa.",
-            "repositorio": "https://github.com/Peixoto1990/organo",
+            "id": 2,
+            "nome": "Pokedex",
+            "capa": "assets/images/projetos-card/pokedex.png",
+            "descricao": "Pokedex é um aplicativo web que consome dados da PokéAPI e os exibe de forma intuitiva e dinâmica na interface. O objetivo é apresentar informações sobre os Pokémon de maneira acessível e visualmente organizada.",
+            "repositorio": "https://github.com/Peixoto1990/js-developer-pokedex",
             "tecnologias": [
                 {
                     "nome": "HTML 5",
@@ -49,18 +49,41 @@ function criaDados() {
                 {
                     "nome": "Javascript",
                     "icone": "assets/images/icons/js.png"
-                },
-                {
-                    "nome": "ReactJS",
-                    "icone": "assets/images/icons/react.png"
                 }
             ],
             "funcionalidades": [
-                "Cadastrar novos colaboradores",
-                "Excluir colaboradores",
-                "Cadastrar novos times"
+                "Buscar informações sobre Pokemons",
+                "Construir o card dos Pokemons",
+                "Exibir modal com informações detalhadas"
             ],
-            "video": "assets/videos/organo-video.webm"
+            "video": "assets/videos/pokedex-video.webm"
+        },
+        {
+            "id": 3,
+            "nome": "Snake Game",
+            "capa": "assets/images/projetos-card/snake.png",
+            "descricao": "Snake é um jogo web desenvolvido com elementos HTML para construir o corpo da cobra e propriedades CSS para criar o movimento. Trata-se da clássica cobrinha, recriada com tecnologias modernas para o navegador.",
+            "repositorio": "https://github.com/Peixoto1990/snake",
+            "tecnologias": [
+                {
+                    "nome": "HTML 5",
+                    "icone": "assets/images/icons/html-5.png"
+                },
+                {
+                    "nome": "CSS 3",
+                    "icone": "assets/images/icons/css-3.png"
+                },
+                {
+                    "nome": "Javascript",
+                    "icone": "assets/images/icons/js.png"
+                }
+            ],
+            "funcionalidades": [
+                "Construir o corpo de cobra",
+                "Verificar colisões com o corpo",
+                "Gerenciar pontos"
+            ],
+            "video": "assets/videos/snake-video.webm"
         }
     ]
 
@@ -79,6 +102,7 @@ function criaCard(data) {
                             ${data.descricao}
                         </p>
                         <ol class="projetos-lista__item--conteudo__lista-funcionalidades">
+                            <h4>Funcionalidades</h4>
                             ${data.funcionalidades.map((funcionalidade) => `<li>${funcionalidade}</li>`).join("")}
                         </ol>
                         <ul class="projetos-lista__item--conteudo__lista-imagens">
